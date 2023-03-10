c8c96e7579870d952fb34750c16048473598ad87
Jai <jaijai.stell@gmail.com>
2023-03-09 19:13:37 -0700

    completed not lab. I totally forgot to push the other error lab

--- errors.js
+++ errors.js
@@ -2,19 +2,23 @@
 
 const snackSelection = 'Pretzels';
 
-if (snackSelection = 'Pretzels') {
+if (snackSelection === 'Pretzels') {
   console.log("That'll be $1, please!");
 }
-
+//added the === instead of the =
 
 // Once the code above this line is fixed you'll be able to see the error from the code below
 
 function first () {
   second();
 }
-
+//line 13
 function second () {
   third();
 }
-
+//line 17
+function third(){
+  console.log("Now I am a function!");
+}
 first();
+//line 20
